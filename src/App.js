89d43fbe16a06b110bb5fd4f4d9f2b31c9d4c689
alemba-21 <alembakiragu@gmail.com>
2023-02-14 16:11:25 +0300

    working on toggle section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import Hero from './components/Hero'
+import Projects from "./components/Projects";
+// import Hero from './components/Hero'
 
 function App() {
   return (
-    <Hero/>
+    // <Hero/>
+    <Projects/>
   );
 }
 
---
+++ src/components/Projects.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='projo'>
+        <div className="navbar">
+            
+        </div>
+        <div className="info2">
+            <h1>Projects</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nam adipisci soluta officia at commodi iure illo natus blanditiis consequuntur ipsa temporibus hic fugit, veritatis recusandae debitis enim sequi magni!</p>
+        </div>
+        <div className="toggle">
+            <button>Designs</button>
+            <button>E-Commerce</button>
+            <button>API Projects</button>
+        </div>
+        <div className="cards"></div>
+    </div>
+  )
+}
+
+export default Projects
