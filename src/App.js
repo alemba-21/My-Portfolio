2f08a3f95daa12f51dc5b62654ac54c608763363
alemba-21 <alembakiragu@gmail.com>
2023-02-17 12:39:08 +0300

    making the cards dynamic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from './components/Hero'
 import Projects from "./components/Projects"
+// import Cards from "./components/Cards";
 // import Designs from "./components/Designs";
 // import Ecommerce from './components/Ecommerce'
 // import API from './components/Api'
@@ -11,6 +12,9 @@ function App() {
     <Routes>
       <Route path="/" element={<Hero />} />
       <Route path="/Projects" element={<Projects/>} />
+      {/* <Route path="/designs-cards" element={<Cards/>} />
+      <Route path="/designs-cards2" element={<Cards/>}/>
+      <Route path="/designs-cards3" element={<Cards/>}/> */}
       {/* <Route path="/Designs" element={<Designs/>} />
       <Route path="/E-Commerce" element={<Ecommerce/>} />
       <Route path="/API" element={<API/>} /> */}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import img1 from '../assets/img1.jpg'
+import {Link, Routes, Route, useNavigate} from 'react-router-dom'
 import{FaTwitter, FaGithub, FaLinkedin, FaHome} from 'react-icons/fa'
 import "./Projects.css"
 
 const Projects = () => {
+
+  const navigate = useNavigate();
+
+  const navigateToDesigns = () => {
+    navigate('/designs-cards');
+  };
+
+  const navigateEcommerce = () => {
+    navigate('/designs-cards2');
+  };
+
+  const navigateApi = () => {
+    navigate('/designs-cards3');
+  };
+
   return (
     <div className='projo'>
         <div className="navbar">
@@ -19,13 +35,28 @@ const Projects = () => {
             <h1>Projects</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nam adipisci soluta officia at commodi iure illo natus blanditiis consequuntur ipsa temporibus hic fugit, veritatis recusandae debitis enim sequi magni!</p>
         </div>
-        <div className="toggle">
-          <Link to='/Designs'><button>Designs</button></Link>
-          <Link to='/E-Commerce'><button>E-Commerce</button></Link>
-          <Link to='/API'><button>API Projects</button></Link>
-        </div>
-        <div className="cards">
-          
+        <div className="toggle" defaultValue="Designs">
+          <Link>
+            <button onClick={navigateToDesigns}>Designs</button>
+          </Link>
+          <Link>
+            <button onClick={navigateEcommerce}>E-Commerce</button>
+          </Link>
+          <Link>
+            <button onClick={navigateApi}>API Projects</button>
+          </Link>
+          <div className="content">
+            <div className="cards">
+              <div className="cards-img">
+                <img src={img1} alt="" />
+              </div>
+              <div>
+                <h3 className='cards-title'>Designs</h3>
+                <p className='cards-descriptions'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, recusandae? Laudantium tenetur nisi, omnis repellat doloremque maiores voluptatum deserunt. Minus corporis a dolor cum labore deserunt porro ipsa laboriosam alias.
+                Id recusandae aperiam harum, laborum molestiae iste deserunt nam soluta dolore dolorem consequatur quia officiis doloribus inventore, fugiat architecto ipsum placeat nulla reprehenderit distinctio dicta! Tempore molestiae dolore debitis assumenda.</p>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
   )
